1fb34ca739cc913490139ae5a381786e61df0b5f
Unknown <vitaliytv@nitralabs.com>
2018-04-23 10:10:44 +0300

    3d secure

--- 3d/js/index.js
+++ 3d/js/index.js
@@ -1,111 +1,148 @@
-var submit = document.querySelector('#submit');
+var payBtn = document.getElementById('pay-btn')
+var nonceGroup = document.querySelector('.nonce-group')
+var nonceInput = document.querySelector('.nonce-group input')
+var payGroup = document.querySelector('.pay-group')
+var modal = document.getElementById('modal')
+var bankFrame = document.querySelector('.bt-modal-body')
+var closeFrame = document.getElementById('text-close')
+var amountInput = document.getElementById('amount')
+// var clientTokenScript = document.getElementById('client-token')
 
-var clientToken = fetch('https://us-central1-nitra-p.cloudfunctions.net/testRuntimeconfig')
-  .then(function(response) {
+var components = {
+  client: null,
+  threeDSecure: null,
+  hostedFields: null
+}
+
+window.fetch('https://us-central1-nitra-p.cloudfunctions.net/testRuntimeconfig')
+  .then(function (response) {
     return response.text()
+  }).then(function (clientToken) {
+    braintree.client.create({
+      authorization: clientToken
+    }, onClientCreate)
   })
 
-clientToken.then(function(clientTokenId) {
-    braintree.client.create({
-      authorization: clientTokenId
-    }, function (err, clientInstance) {
-      braintree.hostedFields.create({
-        client: clientInstance,
-        fields: {
-          number: {
-            selector: '#cc-num',
-            placeholder: 'Credit Card Number',
-            prefill: '4111111111111111'
-          },
-          cvv: {
-            selector: '#cc-cvv',
-            placeholder: 'CVV'
-          },
-          expirationDate: {
-            selector: '#cc-expiration-date',
-            placeholder: 'MM/YY',
-            prefill: '09/20'
-          }
-        },
-        styles: {
-          input: {
-            'font-size': '16px',
-            '-webkit-font-smoothing': 'antialiased'
-          }
-        }
-      }, function (err, hostedFieldsInstance) {
-        
-        hostedFieldsInstance.on('validityChange', function (event) {
-          var allValid = true;
-          var field, key;
-          
-          for (key in event.fields) {
-            if (event.fields[key].isValid === false) {
-              allValid = false;
-              break;
-            }
-          }
-          
-          if (allValid) {
-            submit.removeAttribute('disabled');
-          } else {
-            submit.setAttribute('disabled', 'disabled');
-          }
-        });
-        
-        submit.addEventListener('click', function () {
-          if (err) {
-            console.error(err);
-            return;
-          }
-    
-          hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
-            if (tokenizeErr) {
-              console.error(tokenizeErr);
-              return;
-            }
+function onClientCreate (err, client) {
+  if (err) {
+    console.log('client error:', err)
+    return
+  }
 
-            // If this was a real integration, this is where you would
-            // send the nonce to your server.
-            console.log('Got a nonce: ' + payload.nonce);
+  components.client = client
 
+  braintree.hostedFields.create({
+    client: client,
+    styles: {
+      input: {
+        'font-size': '14px',
+        'font-family': 'monospace'
+      }
+    },
+    fields: {
+      number: {
+        selector: '#hf-number',
+        placeholder: '4111 1111 1111 1111',
+        prefill: '4111111111111111'
+      },
+      cvv: {
+        selector: '#hf-cvv',
+        placeholder: '123',
+        prefill: '123'
+      },
+      expirationDate: {
+        selector: '#hf-date',
+        placeholder: '12 / 34',
+        prefill: '12/34'
+      }
+    }
+  }, onComponent('hostedFields'))
 
-            // Example POST method implementation:
-            fetch("https://us-central1-nitra-p.cloudfunctions.net/checkout", {
-              body: "payment_method_nonce=" + payload.nonce,
-              headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-              },
-              method: "post",
-          })
+  braintree.threeDSecure.create({
+    client: client
+  }, onComponent('threeDSecure'))
+}
 
-          //   postData('https://us-central1-nitra-p.cloudfunctions.net/checkout', {payment_method_nonce: payload.nonce})
-          //     .then(data => console.log(data)) // JSON from `response.json()` call
-          //     .catch(error => console.error(error))
+function onComponent (name) {
+  return function (err, component) {
+    if (err) {
+      console.log('component error:', err)
+      return
+    }
 
-          //   function postData(url, data) {
-          //   // Default options are marked with *
-          //   return fetch(url, {
-          //     body: JSON.stringify(data), // must match 'Content-Type' header
-          //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          //     // credentials: 'same-origin', // include, same-origin, *omit
-          //     headers: {
-          //       'user-agent': 'Mozilla/4.0 MDN Example',
-          //       'content-type': 'application/json'
-          //     },
-          //     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          //     mode: 'cors', // no-cors, cors, *same-origin
-          //     redirect: 'follow', // manual, *follow, error
-          //     referrer: 'no-referrer', // *client, no-referrer
-          //   })
-          //   .then(response => response.json()) // parses response to JSON
-          //   }
-          });
+    components[name] = component
 
+    if (components.threeDSecure && components.hostedFields) {
+      setupForm()
+    }
+  }
+}
 
-          // This is where you would submit payload.nonce to your server
-          // alert('Submit your nonce to your server here!');
-        });
-      });
-    });
+function setupForm () {
+  enablePayNow()
+}
+
+function addFrame (err, iframe) {
+  bankFrame.appendChild(iframe)
+  modal.classList.remove('hidden')
+}
+
+function removeFrame () {
+  var iframe = bankFrame.querySelector('iframe')
+  modal.classList.add('hidden')
+  iframe.parentNode.removeChild(iframe)
+}
+
+function enablePayNow () {
+  payBtn.value = 'Pay Now'
+  payBtn.removeAttribute('disabled')
+}
+
+closeFrame.addEventListener('click', function () {
+  components.threeDSecure.cancelVerifyCard(removeFrame())
+  enablePayNow()
+})
+
+payBtn.addEventListener('click', function (event) {
+  payBtn.setAttribute('disabled', 'disabled')
+  payBtn.value = 'Processing...'
+  console.log('tokeniz')
+  components.hostedFields.tokenize().then(function (payload) {
+    // send payload.nonce to your server
+    console.log('tokenizar:')
+  }).catch(function (err) {
+    payBtn.removeAttribute('disabled');
+    console.error(err);
+  });
+
+  var t = components.hostedFields.tokenize(function (err, payload) {
+
+    if (err) {
+      console.log('tokenization error:', err)
+      enablePayNow()
+      return
+    } else {
+      console.log('tokenization success:', payload)
+    }
+
+    components.threeDSecure.verifyCard({
+      amount: amountInput.value,
+      nonce: payload.nonce,
+      addFrame: addFrame,
+      removeFrame: removeFrame
+    }, function (err, verification) {
+      if (err) {
+        console.log('verification error:', err)
+        enablePayNow()
+        return
+      }
+
+      console.log('verification success:', verification)
+      nonceInput.value = verification.nonce
+      payGroup.classList.add('hidden')
+      payGroup.style.display = 'none'
+      nonceGroup.classList.remove('hidden')
+    })
+  })
+  console.log(t)
 })
