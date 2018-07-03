ab94f8ef74fba1c5e700cc7841138dc8dec08e33
b.rekachinskiy <b.rekachinskiy@nitralabs.com>
2018-07-03 17:12:44 +0300

    [scip ci]

--- 3d/js/index.js
+++ 3d/js/index.js
@@ -4,177 +4,175 @@ var nonceGroup = document.querySelector('.nonce-group')
 var payGroup = document.querySelector('.pay-group')
 var modal = document.getElementById('modal')
 var bankFrame = document.querySelector('.bt-modal-body')
-var closeFrame = document.getElementById('text-close')
+var closeFrame = document.getElementById('close')
 var amountInput = document.getElementById('amount')
 // var clientTokenScript = document.getElementById('client-token')
 var cvvLabel = document.getElementById('label-cvv')
 
 var components = {
-  client: null,
-  threeDSecure: null,
-  hostedFields: null
+    client: null,
+    threeDSecure: null,
+    hostedFields: null
 }
 
 window.fetch('https://us-central1-nitra-p.cloudfunctions.net/testRuntimeconfig')
-  .then(function (response) {
-    return response.text()
-  }).then(function (clientToken) {
+    .then(function (response) {
+        return response.text()
+    }).then(function (clientToken) {
     braintree.client.create({
-      authorization: clientToken
+        authorization: clientToken
     }, onClientCreate)
-  })
+})
 
 function onClientCreate (err, client) {
-  if (err) {
-    console.log('client error:', err)
-    return
-  }
-
-  components.client = client
-
-  braintree.hostedFields.create({
-    client: client,
-    styles: {
-      'input': {
-        'font-size': '16px',
-        'font-family': 'courier, monospace',
-        'font-weight': 'lighter',
-        'color': '#ccc'
-      },
-      ':focus': {
-        'color': 'black'
-      },
-      '.valid': {
-        'color': '#8bdda8'
-      }
-    },
-    fields: {
-      number: {
-        selector: '#hf-number',
-        placeholder: '4111 1111 1111 1111',
-        prefill: '4111111111111111'
-      },
-      cvv: {
-        selector: '#hf-cvv',
-        placeholder: '123',
-        prefill: '123'
-      },
-      expirationDate: {
-        selector: '#hf-date',
-        placeholder: '12 / 34',
-        prefill: '12/34'
-      }
+    if (err) {
+        console.log('client error:', err)
+        return
     }
-  }, function (createErr, hostedFieldsInstance) {
-    components['hostedFields'] = hostedFieldsInstance
-    hostedFieldsInstance.on('cardTypeChange', function (event) {
-      if (event.cards.length === 1) {
-        switch (event.cards[0].type) {
-          case 'american-express':
-          case 'maestro':
-          case 'master-card':
-          case 'visa':
-            console.log(event.cards[0].niceType)
-            cvvLabel.innerHTML = event.cards[0].code['name']
-            break
-          default:
-            alert('Only american-express, maestro, master-card or visa supported')
+
+    components.client = client
+
+
+    braintree.hostedFields.create({
+        client: client,
+        styles: {
+            'input': {
+                // 'font-size': '16px',
+                // 'font-family': 'courier, monospace',
+                // 'font-weight': 'lighter',
+                // 'color': '#ccc'
+            },
+            ':focus': {
+                'color': 'black'
+            },
+        },
+        fields: {
+            number: {
+                selector: '#card-number',
+                placeholder: 'Номер карты',
+                // prefill: '4111111111111111'
+            },
+            cvv: {
+                selector: '#cvv',
+                placeholder: 'cvv',
+                // prefill: '123'
+            },
+            expirationDate: {
+                selector: '#expiration-date',
+                placeholder: "MM / YY",
+                // prefill: '12/34'
+            }
         }
-      } else {
-        console.log('Type of card not yet known')
-      }
+    }, function (createErr, hostedFieldsInstance) {
+        components['hostedFields'] = hostedFieldsInstance
+        hostedFieldsInstance.on('cardTypeChange', function (event) {
+            if (event.cards.length === 1) {
+                switch (event.cards[0].type) {
+                    case 'american-express':
+                    case 'maestro':
+                    case 'master-card':
+                    case 'visa':
+                        console.log(event.cards[0].niceType)
+                        cvvLabel.innerHTML = event.cards[0].code['name']
+                        break
+                    default:
+                        alert('Only american-express, maestro, master-card or visa supported')
+                }
+            } else {
+                console.log('Type of card not yet known')
+            }
+        })
+
+        hostedFieldsInstance.on('validityChange', function (event) {
+            var field = event.fields[event.emittedBy]
+
+            if (event.fields['number'].isValid && event.fields['cvv'].isValid && event.fields['expirationDate'].isValid) {
+                console.log(event.emittedBy, 'is fully valid')
+                enablePayNow()
+            } else {
+                console.log(event.emittedBy, 'is not valid')
+                payBtn.setAttribute('disabled', 'disabled')
+            }
+        })
+        setupForm()
     })
 
-    hostedFieldsInstance.on('validityChange', function (event) {
-      var field = event.fields[event.emittedBy]
+    braintree.threeDSecure.create({
+        client: client
 
-      if (event.fields['number'].isValid && event.fields['cvv'].isValid && event.fields['expirationDate'].isValid) {
-        console.log(event.emittedBy, 'is fully valid')
-        enablePayNow()
-      } else {
-        console.log(event.emittedBy, 'is not valid')
-        payBtn.setAttribute('disabled', 'disabled')
-      }
+    }, function (createErr, threeDSecureInstance) {
+        components['threeDSecure'] = threeDSecureInstance
+        setupForm()
     })
-    setupForm()
-  })
-
-  braintree.threeDSecure.create({
-    client: client
-
-  }, function (createErr, threeDSecureInstance) {
-    components['threeDSecure'] = threeDSecureInstance
-    setupForm()
-  })
 }
 
 function setupForm () {
-  if (components.threeDSecure && components.hostedFields) {
-    payBtn.value = 'Pay Now'
-  }
+    if (components.threeDSecure && components.hostedFields) {
+        payBtn.value = 'Pay Now'
+    }
 }
 
 function addFrame (err, iframe) {
-  bankFrame.appendChild(iframe)
-  modal.classList.remove('hidden')
+    bankFrame.appendChild(iframe)
+    modal.classList.remove('hidden')
 }
 
 function removeFrame () {
-  var iframe = bankFrame.querySelector('iframe')
-  modal.classList.add('hidden')
-  iframe.parentNode.removeChild(iframe)
+    var iframe = bankFrame.querySelector('iframe')
+    modal.classList.add('hidden')
+    iframe.parentNode.removeChild(iframe)
 }
 
 function enablePayNow () {
-  payBtn.removeAttribute('disabled')
+    payBtn.removeAttribute('disabled')
 }
 
 closeFrame.addEventListener('click', function () {
-  components.threeDSecure.cancelVerifyCard(removeFrame())
-  enablePayNow()
+    components.threeDSecure.cancelVerifyCard(removeFrame())
+    enablePayNow()
 })
 
 payBtn.addEventListener('click', function (event) {
-  payBtn.setAttribute('disabled', 'disabled')
-  payBtn.value = 'Processing...'
+    payBtn.setAttribute('disabled', 'disabled')
+    payBtn.value = 'Processing...'
 
-  components.hostedFields.tokenize(function (err, payload) {
-    if (err) {
-      console.error('tokenization error:', err)
-      enablePayNow()
-      return
-    } else {
-      console.log('tokenization success:', payload)
-    }
+    components.hostedFields.tokenize(function (err, payload) {
+        if (err) {
+            console.error('tokenization error:', err)
+            enablePayNow()
+            return
+        } else {
+            console.log('tokenization success:', payload)
+        }
 
-    components.threeDSecure.verifyCard({
-      amount: amountInput.value,
-      nonce: payload.nonce,
-      addFrame: addFrame,
-      removeFrame: removeFrame
-    }, function (err, verification) {
-      if (err) {
-        console.error('verification error:', err)
-        enablePayNow()
-        return
-      }
+        components.threeDSecure.verifyCard({
+            amount: amountInput.value,
+            nonce: payload.nonce,
+            addFrame: addFrame,
+            removeFrame: removeFrame
+        }, function (err, verification) {
+            if (err) {
+                console.error('verification error:', err)
+                enablePayNow()
+                return
+            }
 
-      console.log('verification success:', verification)
-      // nonceInput.value = verification.nonce
-      if (verification.liabilityShifted) {
-        fetch('https://us-central1-nitra-p.cloudfunctions.net/checkout', {
-          body: 'payment_method_nonce=' + verification.nonce + '&email=v@nitra.ai',
-          headers: {
-            'content-type': 'application/x-www-form-urlencoded'
-          },
-          method: 'post'
+            console.log('verification success:', verification)
+            // nonceInput.value = verification.nonce
+            if (verification.liabilityShifted) {
+                fetch('https://us-central1-nitra-p.cloudfunctions.net/checkout', {
+                    body: 'payment_method_nonce=' + verification.nonce + '&email=v@nitra.ai',
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    },
+                    method: 'post'
+                })
+            } else {
+                console.error("Couldn't create 3-D Secure transaction")
+            }
+            payGroup.classList.add('hidden')
+            payGroup.style.display = 'none'
+            nonceGroup.classList.remove('hidden')
         })
-      } else {
-        console.error("Couldn't create 3-D Secure transaction")
-      }
-      payGroup.classList.add('hidden')
-      payGroup.style.display = 'none'
-      nonceGroup.classList.remove('hidden')
     })
-  })
 })
